35c036833a2312a255a72025fd492579b53368d2
Alok <alokgo@gmail.com>
2022-11-10 11:27:06 +0545

    "Added Token Authentication"

--- CatalogService/Program.cs
+++ CatalogService/Program.cs
@@ -1,12 +1,30 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SQLHelper;
 using System.Configuration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Adding Auth Scheme
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- CommonEnitity/Users/AppUser.cs
+++ CommonEnitity/Users/AppUser.cs
@@ -9,10 +9,13 @@ namespace CommonEnitity.Users;
 
 public class AppUser
 {
+    public int UserId { get; set; }
+    public string? DisplayName { get; set; }
     [Required]
     public string UserName { get; set; }
     [Required]
     public string Password { get; set; }
     public string EmailID { get; set; }
+    public DateTime? AddedOn { get; set; }
 }
 
